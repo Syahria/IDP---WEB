e6743371fa528f31a39c6913f2a1d1a8e7ae7c6b
Syahria <syahria687@gmail.com>
2024-05-10 22:44:56 +0800

    project akhir

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="bootstrap-5.3.3/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+  <link rel="stylesheet" href="bootstrap-5.3.3/css/style.css" />
+  <title>FoodMahasiswaSMD</title>
+</head>
+
+<body class="bg-bg-body-tertiary min-vh-100">
+  <nav class="navbar navbar-expand-lg sticky-lg-top py-3" style="background-color: #40A2E3;">
+    <div class="container">
+      <a class="navbar-brand fw-bold text-white " href="#">FoodMahasiswaSMD</a>
+      <button class="navbar-toggler bg-white " type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarScroll">
+        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px">
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container py-3" style="height: 480px;">
+    <div class="h-100 d-flex align-items-center justify-content-center">
+      <div class="col-8">
+        <h1>Selamat Datang Di <span class="fw-bold" style="color: #40A2E3;">Food Mahasiswa Samarinda</span></h1>
+        <hr>
+        <h4>Temukan Tempat Makan Favoritmu Disini !</h4>
+        <p>Food Mahasiswa Samarinda adalah website yang menyediakan informasi tempat makan favorit di Samarinda. Website ini dibuat untuk memudahkan mahasiswa dalam mencari tempat makan yang sesuai dengan selera dan budget mahasiswa.</p>
+        <hr>
+        <a href="pages/login.php" class="btn btn-biru rounded-4 px-5 fw-bold">Masuk</a>
+      </div>
+      <div class="col-4">
+        <img src="images/rumah-makan/default.png" class="card-img-top rounded-top object-fit-contain" style="height: 250px;" alt="<?= $row['gambar'] ?>" />
+      </div>
+    </div>
+  </div>
+  </div>
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 border-top fixed-bottom" style="background-color: #40A2E3;">
+    <div class="col-md-4 d-flex align-items-center ps-5 ">
+      <span class="mb-3 mb-md-0 text-white ">© Syahria - 2024</span>
+    </div>
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex me-5 ">
+      <li class="ms-3">
+        <a class="text-white " href="https://www.instagram.com/"><i class="bi bi-instagram"></i></a>
+      </li>
+      <li class="ms-3">
+        <a class="text-white " href="https://www.facebook.com"><i class="bi bi-facebook"></i></a>
+      </li>
+    </ul>
+  </footer>
+  <script src="bootstrap-5.3.3/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
---
+++ users/edit.php
@@ -0,0 +1,180 @@
+<?php
+include('../components/koneksi.php');
+// Mengambil id dari url
+$id = $_GET['id'];
+// Mengambil data dari database
+$result = mysqli_query($koneksi, "SELECT * FROM users WHERE id = $id");
+// Mengubah data menjadi array assosiatif
+$dataUser = mysqli_fetch_assoc($result);
+
+// Jika data tidak ditemukan, kembali ke index.php
+if (!$dataUser) {
+  echo "<script>alert('Id tidak ditemukan')</script>";
+  echo "<script>window.location.replace('index.php')</script>";
+  exit;
+}
+
+// jika tombol submit ditekan
+if (isset($_POST['submit'])) {
+  // Mengambil data dari form
+  $username = $_POST['username'];
+  $email = $_POST['email'];
+  $role = $_POST['role'];
+  if ($dataUser['id'] == $_SESSION['id']) {
+    $password = $_POST['password'];
+    $konfirmasi_password = $_POST['konfirmasi-password'];
+  }
+  // Check email
+  $cekEmail = "SELECT * FROM users WHERE email = '$email' AND email != '$dataUser[email]'";
+  $result = $koneksi->query($cekEmail);
+  if ($result->num_rows > 0) {
+    echo "<script>alert('Email sudah digunakan')</script>";
+  } else {
+    // Jika password dan konfirmasi password tidak sama
+    if ($password != $konfirmasi_password) {
+      echo "<script>alert('Password dan Konfirmasi Password tidak sama')</script>";
+    } else {
+      // Jika _files kosong
+      if (empty($_FILES['gambar']['name'])) {
+        $gambar = ''; // default image
+      } else {
+        // Jika _files tidak kosong
+        $gambar = $_FILES['gambar']['name'];
+        $tmp = $_FILES['gambar']['tmp_name'];
+        $path = "../images/users/" . $gambar;
+      }
+      // jika gambar tidak diubah
+      if ($gambar == '') {
+        if ($password == '') {
+          // Jika password kosong, tidak ubah password
+          $query = "UPDATE users SET username = '$username', email = '$email', role = '$role' WHERE id = $id";
+        } else {
+          // Jika password tidak kosong, ubah password
+          $password = md5($password);
+          $query = "UPDATE users SET username = '$username', email = '$email', role = '$role', password = '$password' WHERE id = $id";
+        }
+        // Menjalankan query
+        mysqli_query($koneksi, $query);
+      } else {
+        // Jika gambar diubah
+        if ($password == '') {
+          // Jika password kosong, tidak ubah password
+          $query = "UPDATE users SET username = '$username', email = '$email', role = '$role', gambar = '$gambar' WHERE id = $id";
+        } else {
+          // Jika password tidak kosong, ubah password
+          $password = md5($password);
+          $query = "UPDATE users SET username = '$username', email = '$email', role = '$role', password = '$password', gambar = '$gambar' WHERE id = $id";
+        }
+        move_uploaded_file($tmp, '../images/users/' . $gambar);
+        // Menjalankan query
+        mysqli_query($koneksi, $query);
+      }
+    }
+    echo "<script>alert('Data berhasil diubah')</script>";
+    echo "<script>window.location.replace('index.php')</script>";
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <?php include('../components/head.php') ?>
+  <title>Data Pengguna</title>
+</head>
+
+<body>
+  <div class="container-fluid">
+    <div class="row min-vh-100">
+      <?php include('../components/sidebar.php') ?>
+      <div class="col-9 p-0">
+        <?php include('../components/navbar.php') ?>
+        <div id="content" class="px-5 py-3 z-1" style="z-index: 1;">
+          <!-- start content -->
+          <h4 class="fw-bold">Ubah Data</h4>
+          <div class="card bg-body-secondary p-4">
+            <div class="row container-fluid">
+              <div class="card-head col-4 text-center p-3">
+                <img id="preview" src="../images/users/<?= $dataUser['gambar'] ?>" width="200" height="200" class="bg-white rounded-circle  object-fit-contain" alt="">
+              </div>
+              <div class="card-body col-8">
+                <form class="form" action="" method="post" enctype="multipart/form-data">
+                  <div class="mb-2">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" class="form-control" id="username" placeholder="Username" name="username" value="<?php echo $dataUser['username']; ?>">
+                  </div>
+                  <div class="mb-2">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email" placeholder="Email" name="email" value="<?php echo $dataUser['email']; ?>">
+                  </div>
+                  <div class="mb-2">
+                    <label for="role" class="form-label">Role</label>
+                    <select class="form-control" id="role" name="role">
+                      <option value="user" <?php if ($dataUser['role'] == 'user') echo 'selected'; ?>>User</option>
+                      <option value="admin" <?php if ($dataUser['role'] == 'admin') echo 'selected'; ?>>Admin</option>
+                    </select>
+                  </div>
+                  <?php if ($dataUser['id'] == $_SESSION['id']) { ?>
+                    <div class="mb-2">
+                      <div class="row px-3">
+                        <div class="col-6">
+                          <label for="password" class="form-label">Password</label>
+                          <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                        </div>
+                        <div class=" col-6">
+                          <label for="konfirmasi-password" class="form-label">Konfirmasi Password</label>
+                          <input type="password" class="form-control" id="konfirmasi-password" name="konfirmasi-password" placeholder="Konfirmasi Password">
+                        </div>
+                      </div>
+                    </div>
+                  <?php } ?>
+                  <div class="mb-5">
+                    <label for="formFile" class="form-label">Gambar</label>
+                    <input class="form-control" type="file" name="gambar" id="formFile">
+                  </div>
+                  <button class="btn btn-biru btn-lg w-100" type="submit" name="submit">Simpan</button>
+                </form>
+              </div>
+            </div>
+          </div>
+          <!-- end content -->
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php include('../components/footer.php') ?>
+  <script>
+    document.getElementById('formFile').addEventListener('change', function(event) {
+      var reader = new FileReader();
+      reader.onload = function() {
+        var output = document.getElementById('preview');
+        output.src = reader.result;
+      }
+      reader.readAsDataURL(event.target.files[0]);
+    });
+  </script>
+  <script>
+    $(document).ready(function() {
+      $('#dataTables').DataTable({
+        aaSorting: [],
+        "columnDefs": [{
+          className: "dt-head-center",
+          targets: "_all"
+        }],
+        "preDrawCallback": function(settings) {
+          $('#dataTables tbody').hide();
+        },
+        "drawCallback": function() {
+          $('#dataTables tbody td').addClass("blurry");
+          $('#dataTables tbody').fadeIn(200);
+          setTimeout(function() {
+            $('#dataTables tbody td').removeClass("blurry");
+          }, 200);
+        }
+      });
+    });
+  </script>
+</body>
+
+</html>
